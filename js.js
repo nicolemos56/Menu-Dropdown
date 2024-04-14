880cb901c5446c931a958932e5afd68e09937b2f
Manuel <nicocohen56@gmail.com>
2024-04-14 15:27:32 +0100

    dropdown adicionado

--- js.js
+++ js.js
@@ -1,18 +1,17 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  
-  window.onmouseenter = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+function minhafuncao(){
+  const menudropdown = document.getElementById("idMenuDropdown");
+  menudropdown.classList.toggle("show"); 
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.btndrop')) {
+    var dropdowns = document.getElementsByClassName("menu-dropdown");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
-  
+}
